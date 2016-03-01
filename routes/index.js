c63292deb967f06d7f77f6a7fe412608ba3428e1
Lionel T <elrumordelaluz@hotmail.com>
2016-03-02 00:18:40 +0100

    Add user auth

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,11 @@ var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
 
+var jwt    = require('jsonwebtoken');
+
 // our db model
 var Animal = require("../models/model.js");
+var User = require("../models/user.js");
 
 /**
  * GET '/'
@@ -255,5 +258,103 @@ router.get('/api/delete/:id', function(req, res){
   })
 
 })
+
+
+/////////////////////
+/////////////////////
+/////////////////////
+/////////////////////
+
+// route middleware to verify a token
+router.use(function(req, res, next) {
+
+  // check header or url parameters or post parameters for token
+  var token = req.body.token || req.query.token || req.headers['x-access-token'];
+
+  // decode token
+  if (token) {
+
+    // verifies secret and checks exp
+    jwt.verify(token, 'ilovescotchyscotch', function(err, decoded) {      
+      if (err) {
+        return res.json({ success: false, message: 'Failed to authenticate token.' });    
+      } else {
+        // if everything is good, save to request for use in other routes
+        req.decoded = decoded;    
+        next();
+      }
+    });
+
+  } else {
+
+    // if there is no token
+    // return an error
+    return res.status(403).send({ 
+        success: false, 
+        message: 'No token provided.' 
+    });
+    
+  }
+});
+
+router.get('/setup', function(req, res) {
+  // create a sample user
+  var nick = new User({ 
+    name: 'Nick Cerminara', 
+    password: 'password',
+    admin: true 
+  });
+  // save the sample user
+  nick.save(function(err) {
+    if (err) throw err;
+    console.log('User saved successfully');
+    res.json({ success: true });
+  });
+});
+
+// route to return all users (GET http://localhost:8080/api/users)
+router.get('/api/users', function(req, res) {
+  User.find({}, function(err, users) {
+    res.json(users);
+  });
+});
+
+// route to authenticate a user (POST http://localhost:8080/api/authenticate)
+router.post('/api/authenticate', function(req, res) {
+
+  // find the user
+  User.findOne({
+    name: req.body.name
+  }, function(err, user) {
+
+    if (err) throw err;
+
+    if (!user) {
+      res.json({ success: false, message: 'Authentication failed. User not found.' });
+    } else if (user) {
+
+      // check if password matches
+      if (user.password != req.body.password) {
+        res.json({ success: false, message: 'Authentication failed. Wrong password.' });
+      } else {
+
+        // if user is found and password is right
+        // create a token
+        var token = jwt.sign(user, 'ilovescotchyscotch', { // TODO: communicate with ENV (app.get('superSecret'))
+          expiresInMinutes: 1440 // expires in 24 hours
+        });
+
+        // return the information including token as JSON
+        res.json({
+          success: true,
+          message: 'Enjoy your token!',
+          token: token
+        });
+      }   
+
+    }
+
+  });
+});
 
 module.exports = router;
