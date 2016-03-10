0c6c4583f344bc793440b5d0f12e593e0dd7ae6f
Lionel T <elrumordelaluz@hotmail.com>
2016-03-10 11:10:21 +0100

    Allow CORS

--- app.js
+++ app.js
@@ -24,6 +24,21 @@ app.set('view engine', 'html');
 app.set('layout','layout');
 app.engine('html', require('hogan-express'));;
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+
+app.use(allowCrossDomain)
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
