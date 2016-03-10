1eaecc1162bd6bf6648ddc7a5f9d5fc18c775f05
Lionel T <elrumordelaluz@hotmail.com>
2016-03-10 10:35:39 +0100

    Update favico[router]

--- app.js
+++ app.js
@@ -24,8 +24,7 @@ app.set('view engine', 'html');
 app.set('layout','layout');
 app.engine('html', require('hogan-express'));;
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
