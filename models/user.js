c220b0e7a971c1cd535c90a2da409057c96fcb10
Lionel T <elrumordelaluz@hotmail.com>
2016-05-06 15:07:35 +0200

    Implement new Tokenization

--- models/user.js
+++ models/user.js
@@ -1,10 +1,66 @@
-// get an instance of mongoose and mongoose.Schema
 var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
+var bcrypt = require('bcrypt-nodejs');
 
-// set up a mongoose model and pass it using module.exports
-module.exports = mongoose.model('User', new Schema({ 
-    name: String, 
-    password: String, 
-    admin: Boolean 
-}));
+var SALT_FACTOR = 10;
+
+var userSchema = mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  admin: Boolean,
+  token: String
+});
+
+var noop = function () {
+
+};
+
+userSchema.pre('save', function(done) {
+  var user = this;
+
+  if (!user.isModified('password')) {
+    return done();
+  }
+
+  bcrypt.genSalt(SALT_FACTOR, function(err, salt) {
+    if (err) { return done(err); }
+
+    bcrypt.hash(user.password, salt, noop, function(err, hashedPassword) {
+      if (err) { return done(err); }
+      user.password = hashedPassword;
+      done();
+    });
+  });
+});
+
+userSchema.methods.checkPassword = function(guess, done) {
+  bcrypt.compare(guess, this.password, function(err, isMatch) {
+    done(err, isMatch);
+  });
+};
+
+userSchema.methods.name = function () {
+  return this.username;
+}
+
+var User = mongoose.model('User', userSchema);
+
+module.exports = User;
+
+// var mongoose = require('mongoose');
+// var Schema = mongoose.Schema;
+// module.exports = mongoose.model('User', new Schema({
+//     name: String,
+//     password: String,
+//     admin: Boolean
+// }));
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,93 @@
 var express = require('express');
+var passport = require('passport');
+var User = require('../models/user');
+var jwt = require('jsonwebtoken');
+var ms = require('ms');
+
 var router = express.Router();
 
 const title = 'Orion API';
 
-router.get('/', function(req, res) {
+router.use(function(req, res, next) {
+  res.locals.currentUser = req.user;
+  res.locals.errors = req.flash('error');
+  res.locals.infos = req.flash('info');
+  next();
+});
+
+function ensureAuthenticated (req, res, next) {
+  if (req.isAuthenticated()) {
+    next();
+  } else {
+    req.flash('info', 'You must be logged in to see this page.');
+    res.redirect('/login');
+  }
+}
+
+router.get('/', function(req, res, next) {
   res.render('Index', { title });
+});
+
+router.get('/signup', function(req, res) {
+  // res.render('Signup', { title });
+  res.redirect('/')
 });
 
 router.get('/login', function(req, res) {
   res.render('Login', { title });
+});
+
+router.get('/logout', function(req, res) {
+  req.logout();
+  res.redirect('/');
+});
+
+router.post('/login', passport.authenticate('login', {
+  successRedirect: '/',
+  failureRedirect: '/login',
+  failureFlash: true
+}));
+
+router.post('/signup', function(req, res, next) {
+  var username = req.body.username;
+  var password = req.body.password;
+
+  User.findOne({ username: username }, function(err, user) {
+    if (err) { return next(err); }
+
+    if (user) {
+      req.flash('error', 'User already exists.');
+      return res.redirect('/signup');
+    }
+
+    var newUser = new User({
+      username: username,
+      password: password
+    });
+
+    newUser.save(next);
+
+  });
+}, passport.authenticate('login', {
+  successRedirect: '/',
+  failureRedirect: '/signup',
+  failureFlash: true
+}));
+
+router.get('/token', ensureAuthenticated, function(req, res, next) {
+  var profile = { name: req.user.username, password: req.user.password };
+  var token = jwt.sign(profile, process.env.SECRET);
+
+  req.user.token = token;
+  req.user.save(function(err) {
+    if (err) {
+      next(err);
+      return;
+    }
+    req.flash('info', 'Token generated successfully!');
+    res.redirect('/');
+  });
+  // next();
 });
 
 router.use(function(req, res) {
