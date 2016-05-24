353ff3a678ab52884f3922127b5a83da81d4e348
Lionel T <elrumordelaluz@hotmail.com>
2016-05-24 18:41:04 +0200

    Add icons view

---
+++ views/Icons.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import DefaultLayout from './layouts/DefaultLayout';
+
+const Icons = (props) => {
+  return (
+    <DefaultLayout {...props}>
+      <h1>
+        Icons
+      </h1>
+
+
+
+    </DefaultLayout>
+  );
+}
+
+module.exports = Icons;
