e22b03f81f5d8c529e352adb7ba5d028c60ddabf
Lionel T <elrumordelaluz@hotmail.com>
2016-07-14 12:42:47 +0200

    Edit Styles

--- views/Icons.jsx
+++ views/Icons.jsx
@@ -5,32 +5,37 @@ import Element from './Element';
 const Icons = (props) => {
   const renderIcons = () => props.icons.map(icon => {
     return (
-      <div className="ae-grid__item item-sm-6">
+      <div className="edit-icon__item">
         <div className="panel panel-default">
-          <div className="panel-heading clearfix">
-            <h3 className="panel-title pull-left">{icon.name}</h3>
-            <div className="btn-group pull-right" role="group">
-              <a href={`/edit/${icon.iconSlug}`} className="btn btn-info">Edit</a>
+          <header className="panel-heading edit-icon__header">
+            <h3 className="panel-title">
+              {icon.name}{' '}
+              { icon.premium && (
+                <span className="label label-warning">Premium</span>
+              )}
+            </h3>
+            <div className="btn-group" role="group">
+              <a href={`/edit/${icon.iconSlug}`} className="btn">Edit</a>
               <a 
                 href={`/delete/${icon.iconSlug}`} 
                 data-icon={icon.iconSlug}
-                className="btn btn-danger deleteIconButton">Delete</a>
+                className="btn deleteIconButton">Delete</a>
             </div>
-          </div>
+          </header>
           <div className="panel-body">
             <div className="ae-grid">
               <div className="ae-grid__item item-sm-6">
-                <Element obj={icon.paths} />
+                <div className="edit-icon__icon">
+                  <Element obj={icon.paths} />
+                </div>
               </div>
               <div className="ae-grid__item item-sm-6">
-                <p>Slug: <code>{icon.iconSlug}</code></p>
-                <p>Style: <code>{icon.style}</code></p>
-                <p>Premium: <code>{icon.premium ? 'yes' : 'no'}</code></p>
-                <div>Tags:
-                  <ul className="IconTags">
-                    {icon.tags.map(tag => <li key={tag}><code>{tag}</code></li>)}
-                  </ul>
-                </div>
+                <p><b>iconSlug</b> <mark>{icon.iconSlug}</mark></p>
+                <p><b>style</b>: <mark>{icon.style}</mark></p>
+                <b>tags</b>:
+                <ul className="IconTags">
+                  {icon.tags.map(tag => <li><span className="label label-info" key={tag}>{tag}</span></li>)}
+                </ul>
               </div>
             </div>
             <div className="ae-grid">
@@ -43,12 +48,7 @@ const Icons = (props) => {
               </div>
             </div>
           </div>
-          <div className="panel-footer">
-            
-          </div>
-
-
-          
+          <footer className="panel-footer"></footer>
         </div>
       </div>
   )});
@@ -57,14 +57,17 @@ const Icons = (props) => {
 
   return (
     <DefaultLayout {...props}>
-      <div className="page-header">
+      <input type="checkbox" id="edit-layout" />
+      <header className="edit-page__header">
         <h1>
           <a href="/admin" className="btn btn-info btn-xs">&lt;</a>{' '}
-          {mainTitle} <small className="badge">{props.icons.length} icons</small></h1>
-      </div>
-
-
-      <div className="ae-grid">
+          {mainTitle} <small className="badge">{props.icons.length} icons</small>
+        </h1>
+        <label htmlFor="edit-layout" className="edit-layout">
+          view as
+        </label>
+      </header>
+      <div className="edit-icon">
         { renderIcons() }
       </div>
     </DefaultLayout>
